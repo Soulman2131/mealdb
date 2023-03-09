8e2d4fcfbae6d98bf18933d2a51fb32674f6c22b
Soulman2131 <sdrame81@gmail.com>
2023-03-09 11:50:41 +0100

    Fin projet

--- src/App.js
+++ src/App.js
@@ -5,23 +5,36 @@ import Cook from './Cook';
 
 function App() {
   const [meals, setMeals] = useState([]);
+  const [content, setContent] = useState('');
 
   useEffect(() => {
-    axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=Chicken').then(res => {
+    axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${content} `).then(res => {
       console.log(res.data.meals);
       setMeals(res.data.meals);
     });
-  }, []);
+  }, [content]);
 
   return (
     <>
       <div className="app-container">
-        <h1>React Cook</h1>
-        <input type="text" placeholder="Tapez le nom d'un aliment" />
+        <h1>Recette de cuisine</h1>
+        <input
+          type="text"
+          placeholder="Tapez le nom d'un aliment"
+          onChange={e => setContent(e.target.value)}
+        />
         <div className="meals-container">
-          {meals.map((meal, index) => (
-            <Cook key={index} meal={meal} />
-          ))}
+          {meals ? (
+            <>
+              {meals.slice(0, 24).map((meal, index) => (
+                <Cook key={index} meal={meal} />
+              ))}
+            </>
+          ) : (
+            <>
+              <p>Cette recette n'existe pas </p>
+            </>
+          )}
         </div>
       </div>
     </>
